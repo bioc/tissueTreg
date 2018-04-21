176feb0e4f312ce197e95aae177b39500be3f04e
Charles Imbusch <c.imbusch@dkfz.de>
2018-04-21 18:15:12 +0200

    script to update bsseq objects

---
+++ inst/scripts/make-data-update.R
@@ -0,0 +1,15 @@
+# convert data to new bsseq object which has a newer DelayedArray version
+
+library(ExperimentHub)
+library(bsseq)
+
+eh <- ExperimentHub()
+BS.obj.ex.fit <- eh[["EH1072"]]
+BS.obj.ex.fit2 <- BSseq(M = assays(BS.obj.ex.fit)[["M"]], Cov = assays(BS.obj.ex.fit)[["Cov"]], gr = rowRanges(BS.obj.ex.fit))
+BS.obj.ex.fit2_smoothed <- BSmooth(BS.obj.ex.fit2, mc.cores=12, verbose=TRUE, parallelBy = "chr")
+saveRDS(object = BS.obj.ex.fit, file = "treg_twgbs_per_sample.rds")
+
+BS.obj.ex.fit.combined <- eh[["EH1073"]]
+BS.obj.ex.fit.combined2 <- BSseq(M = assays(BS.obj.ex.fit.combined)[["M"]], Cov = assays(BS.obj.ex.fit.combined)[["Cov"]], gr = rowRanges(BS.obj.ex.fit.combined))
+BS.obj.ex.fit.combined_smoothed <- BSmooth(BS.obj.ex.fit.combined2, mc.cores=12, verbose=TRUE, parallelBy = "chr")
+saveRDS(object = BS.obj.ex.fit, file = "treg_twgbs_per_group.rds")
